18d713726b44d4691865fd23c21f23d1193d13d3
Marc <mg.gocon@gmail.com>
2025-05-15 22:12:01 +0800

    Complex Layering

--- Assets/Scripts/LayerTrigger.cs
+++ Assets/Scripts/LayerTrigger.cs
@@ -8,6 +8,23 @@ public class LayerTrigger : MonoBehaviour
 {
     public string layer;
     public string sortingLayer;
+    public Collider2D[] mountainColliders;
+    public Collider2D[] boundaryColliders;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {            
+            foreach (Collider2D mountain in mountainColliders)
+            {
+                mountain.enabled = false;
+            }
+            foreach (Collider2D boundary in boundaryColliders)
+            {
+                boundary.enabled = true;
+            }
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D other)
     {
---
+++ Assets/Scripts/ReEnableBoundary.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReEnableBoundary : MonoBehaviour
+{
+    public Collider2D[] mountainColliders;
+    public Collider2D[] boundaryColliders;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {            
+            foreach (Collider2D mountain in mountainColliders)
+            {
+                mountain.enabled = true;
+            }
+            foreach (Collider2D boundary in boundaryColliders)
+            {
+                boundary.enabled = false;
+            }
+        }
+    }
+}
