1bd80113e7b72e460e4b33e3fa0ad98b84599e0a
mgocon <mg.gocon@gmail.com>
2025-05-18 18:35:32 +0800

    Scene Changes

--- Assets/Scripts/ReEnableBoundary.cs
+++ Assets/Scripts/ReEnableBoundary.cs
@@ -7,6 +7,21 @@ public class ReEnableBoundary : MonoBehaviour
     public Collider2D[] mountainColliders;
     public Collider2D[] boundaryColliders;
 
+    private void Start()
+    {
+        // Ensure mountain colliders are disabled at the start
+        foreach (Collider2D mountain in mountainColliders)
+        {
+            mountain.enabled = false;
+        }
+
+        // Ensure boundary colliders are enabled at the start
+        foreach (Collider2D boundary in boundaryColliders)
+        {
+            boundary.enabled = true;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
